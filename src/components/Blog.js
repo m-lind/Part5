fb8332c16a58b7539af69b8959e9e9e80221eb15
m-lind <mari.j.lindstedt@gmail.com>
2023-08-10 11:58:50 +0300

    Added a button that shows or hides blog details when clicked

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const showWhenVisible = { display: detailsVisible ? "" : "none" };
+
+  const buttonLabel = detailsVisible ? "hide" : "view";
+
+  const toggleVisibility = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => toggleVisibility()}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
