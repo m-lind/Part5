1605dfa17e3e5d6ea8a3f925a1a7d8bea6a43144
m-lind <mari.j.lindstedt@gmail.com>
2023-08-11 13:26:59 +0300

    Added functionality for like button

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const App = () => {
   const blogForm = () => (
     <div>
       {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} setBlogs={setBlogs} />
       ))}
     </div>
   );
