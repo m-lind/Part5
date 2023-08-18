fb8871d7f38aaa9bfc7261d4d24f32f5b4952f05
m-lind <mari.j.lindstedt@gmail.com>
2023-08-18 13:17:34 +0300

    Added a test to make sure the view button works

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -23,3 +23,45 @@ test("renders title", () => {
   const element = screen.getByText("Test title", { exact: false });
   expect(element).toBeDefined();
 });
+
+test("url, number of likes and user is shown when view button is pressed", async () => {
+  const blog = {
+    title: "Test title",
+    author: "Test author",
+    url: "test.url",
+    likes: 5,
+    user: {
+      name: "Test user",
+      username: "testusername",
+    },
+  };
+  const blogs = [];
+  const setBlogs = () => {};
+  const user = { username: "testusername" };
+
+  const mockHandler = jest.fn();
+
+  render(
+    <Blog
+      blog={blog}
+      blogs={blogs}
+      setBlogs={setBlogs}
+      user={user}
+      toggleVisibility={mockHandler}
+    />
+  );
+
+  const userSetup = userEvent.setup();
+  const button = screen.getByText("view");
+  await userSetup.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
+
+  const urlElement = screen.getByText(blog.url);
+  const likesElement = screen.getByText(`likes ${blog.likes}`);
+  const userElement = screen.getByText(blog.user.name);
+
+  expect(urlElement).toBeInTheDocument();
+  expect(likesElement).toBeInTheDocument();
+  expect(userElement).toBeInTheDocument();
+});
