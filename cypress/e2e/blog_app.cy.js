253f857582ace7914c92416b73ad8e4f46ec011a
m-lind <mari.j.lindstedt@gmail.com>
2023-09-15 15:27:18 +0300

    Added a test for like button behaviour, added custom cypress commands and base urls for frontend and backend

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,17 +1,19 @@
+import "../support/commands";
+
 describe("Blog", function () {
   beforeEach(function () {
-    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", `${Cypress.env("BACKEND")}/testing/reset`);
     const user = {
       name: "Matti Luukkainen",
       username: "mluukkai",
       password: "salainen",
     };
-    cy.request("POST", "http://localhost:3003/api/users", user);
-    cy.visit("http://localhost:3000/");
+    cy.request("POST", `${Cypress.env("BACKEND")}/users`, user);
+    cy.visit("");
   });
 
   it("front page can be opened", function () {
-    cy.visit("http://localhost:3000/");
+    cy.visit("");
     cy.contains("Log in");
   });
 
@@ -33,9 +35,7 @@ describe("Blog", function () {
 
   describe("when logged in", function () {
     beforeEach(function () {
-      cy.get("#username").type("mluukkai");
-      cy.get("#password").type("salainen");
-      cy.get("#login-button").click();
+      cy.login({ username: "mluukkai", password: "salainen" });
     });
 
     it("a new blog can be created", function () {
@@ -46,6 +46,33 @@ describe("Blog", function () {
       cy.get("#create-button").click();
       cy.contains("Test title");
       cy.contains("Test author");
+    });
+
+    describe("when a blog exists", function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: "First test",
+          author: "First Author",
+          url: "www.firsttest.fi",
+        });
+        cy.createBlog({
+          title: "Second test",
+          author: "Second Author",
+          url: "www.secondtest.fi",
+        });
+        cy.createBlog({
+          title: "Third test",
+          author: "Third Author",
+          url: "www.thirdtest.fi",
+        });
+      });
+
+      it("it can be liked", function () {
+        cy.contains("Second test").contains("view").click();
+        cy.contains("Second test").parent().as("theParent");
+        cy.get("@theParent").find("#like-button").click();
+        cy.get("@theParent").contains("likes 1");
+      });
     });
   });
 });
